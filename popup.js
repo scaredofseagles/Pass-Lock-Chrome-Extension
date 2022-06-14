0fe260fa3ceb5924e45c8faf98e502b4a0e006ca
Dailey Kaze <dkaze@Daileys-MacBook-Air.local>
2022-06-14 15:47:10 +0200

    testing things out

--- popup.js
+++ popup.js
@@ -1,19 +1,41 @@
-let changeColour = document.getElementById("changeColour");
+// let changeColour = document.getElementById("changeColour");
 
-chrome.storage.sync.get("color", ({colour}) => {
-    changeColour.style.backgroundColor = colour
+// chrome.storage.sync.get("color", ({colour}) => {
+//     changeColour.style.backgroundColor = colour
+// });
+
+// changeColour.addEventListener("click", async () =>{
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         function: setPageBackgroundColor,
+//     });
+// });
+
+// const setPageBackgroundColor = () => {
+//     chrome.storage.sync.get("color", ({ color }) => {
+//         document.body.style.backgroundColor = color;
+//     });
+// }
+
+let addPassBtn = document.getElementById("addPassBtn");
+
+addPassBtn.addEventListener("click", () => {
+	chrome.scripting.executeScript({
+		target: { tabId },
+		function: injectAcctDetails,
+	});
 });
 
-changeColour.addEventListener("click", async () =>{
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: setPageBackgroundColor,
-    });
-});
+const injectAcctDetails = () => {
+	let emailElementsList = document.getElementsByName("email");
+	let passwordElement = document.querySelectorAll("input[type=password]");
+	let emailElement;
 
-const setPageBackgroundColor = () => {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
-}
+	emailElementsList.forEach(elem => {
+		if (elem.tagName === "input") emailElement = elem;
+	});
+
+	emailElement.value = "test@test.com";
+	passwordElement = "test123!@#";
+};
